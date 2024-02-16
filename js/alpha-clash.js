a473424f0117b7357700423942ab912070f187d1
Muhammad-Abrar <tahseenbdserver@outlook.com>
2024-02-16 09:35:23 +0600

    added Change Home Screen To Playground By Click

---
+++ js/alpha-clash.js
@@ -0,0 +1,6 @@
+function play() {
+  const play = document.getElementById("play");
+  play.classList.add("hidden");
+  const mainGame = document.getElementById("mainGame")
+  mainGame.classList.remove("hidden")
+}
